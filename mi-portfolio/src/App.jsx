9118e3461398d4475c919e21dc6e59cf8d2a63f4
XxJeefreexX <lpena8133@gmail.com>
2026-03-20 00:06:19 -0400

    Nuevo componente "Hero"
    Se creó el directorio de componentes y un nuevo componente llamado "Hero"

--- mi-portfolio/src/App.jsx
+++ mi-portfolio/src/App.jsx
@@ -1,26 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Hero from './components/Hero.jsx';
 
 function App() {
   return (
-    <>
-      <header>
-        <div className="container">
-          <div className="hero">
-            <div className="hero-text">
-              <h1>Hola, soy un desarrollador web</h1>
-              <p>Me especializo en crear sitios web modernos y responsivos.</p>
-              <a href="#contact" className="btn">Contáctame</a>
-            </div>
-            <div className="hero-image">
-              <img src={heroImg} alt="Hero" />
-            </div>
-          </div>
-        </div>
-      </header>
-    </>
+    <main>
+      <Hero />
+    </main>
   )
 }
 
